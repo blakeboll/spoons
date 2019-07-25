0cdb5139a026f2a5e834cf34017173703155d08c
blakeboll <blake@webscalenetworks.com>
2019-07-25 14:10:30 -0600

    added the hiss detector

--- init.lua
+++ init.lua
@@ -60,13 +60,10 @@ function mouseHighlight()
 end
 hs.hotkey.bind({"cmd","alt","shift"}, "D", mouseHighlight)
 
+--attempting to send a text messsage to myself
+hs.hotkey.bind({"cmd","alt","shift"}, "T", function()
 
---Sends text messages
-function sendText()
-    hs.messages.iMessage("emailhere@email.email", "Testing")
-    hs.messages.SMS("+insertanumberhere", "Test Two")
-end
-hs.hotkey.bind({"cmd","alt","shift"}, "T", sendText)
+end)
 
 --These detect changes in the wifi?????????????
 -- wifiWatcher = hs.wifi.watcher.new(ssidChanged)
@@ -76,7 +73,6 @@ hs.hotkey.bind({"cmd","alt","shift"}, "T", sendText)
 -- keycode enterer
 function updater(updateString)
     updateString:gsub(".", function(c)
-        print(c)
         secondKey = false
         if c == ' ' then
             c = "space"
@@ -97,22 +93,64 @@ function updater(updateString)
     hs.eventtap.event.newKeyEvent("return", false):post()
 end
 -- install both the frontend and backend
-hs.hotkey.bind({"cmd","alt","shift"}, "R", function()
-    print('3')
+hs.hotkey.bind({"cmd","alt","shift"}, "M", function()
     updateString = "sudo apt-get update && sudo apt-get install -y webscale-portal && sudo apt-get install lgcommandprocessor"
+    print('updating, and installing portal and control')
     updater(updateString)
 end)
 -- only install the frontend
-hs.hotkey.bind({"cmd","alt","shift"}, "W", function()
-    print('4')
+hs.hotkey.bind({"cmd","alt","shift"}, "P", function()
     updateString = "sudo apt-get update && sudo apt-get install -y webscale-portal"
+    print('updating, and installing portal')
     updater(updateString)
 end)
 -- only install the backend
-hs.hotkey.bind({"cmd","alt","shift"}, "E", function()
-    print('5')
+hs.hotkey.bind({"cmd","alt","shift"}, "L", function()
     updateString = "sudo apt-get update && sudo apt-get install lgcommandprocessor"
+    print('updating, and installing control')
     updater(updateString)
 end)
 
+-- a spoon process that recognizes when you hiss sssssssssss and then does stuff
+local popclick = require "hs.noises"
+listener = nil
+popclickListening = false
 
+function noise1()
+    print('I can hear you sound 1')
+end
+
+function noise2()
+    print('I can hear you sound 2')
+end
+
+function noise3()
+    print('I can hear you sound 3')
+end
+
+
+hs.hotkey.bind({"cmd","alt","shift"}, "B", function()
+    if not popclickListening then
+        listener:start()
+        hs.alert.show("listening")
+    else
+        listener:stop()
+        hs.alert.show("stopped listening")
+    end
+    popclickListening = not popclickListening
+end)
+
+function popclickInit()
+    popclickListening = false
+    listener = popclick.new(function(soundType)
+        if soundType == 1 then
+            noise1()
+        elseif soundType == 2 then
+            noise2()
+        elseif soundType == 3 then
+            noise3()
+        end
+    end)
+end
+
+popclickInit()
